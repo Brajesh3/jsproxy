1d083b8363fd9c554753171285631aa72a7897b2
Brajesh <95830772+Brajesh3@users.noreply.github.com>
2024-05-01 02:58:38 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -88,8 +88,8 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
-      redir: 'https://blog.51sec.org/'
+    'https://blog.com/': {
+      redir: 'https://brajesh.site/'
     },
     'http://haha.com/': {
       content: 'Hello World'
